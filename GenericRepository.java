5063ac97e2ca249affbc1fb23c75abb69ba74240
mthScarpatti <59294125+mthScarpatti@users.noreply.github.com>
2019-12-27 17:50:44 -0300

    Delete GenericRepository.java

--- GenericRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.empresa.app.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface GenericRepository<T, ID> extends JpaRepository<T, ID> {
-
-}
